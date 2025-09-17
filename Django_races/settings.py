c5398725d8a6d3605605e477228c7fa35b991515
Kirill-Marushko <k.v.marushko2001@gmail.com>
2025-09-17 17:21:58 +0200

    update

--- Django_races/settings.py
+++ Django_races/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zeuzs@2(t-#4t3ds1%jwh=woa9k^w(cxarob%$v%@dv0aqr+bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["races-f1.pp.ua"]
+ALLOWED_HOSTS = ["races-f1.pp.ua", '127.0.0.1']
 # ALLOWED_HOSTS = []
 
 
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
